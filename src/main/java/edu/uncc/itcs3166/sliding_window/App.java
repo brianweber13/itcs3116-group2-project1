a41d148bba1e6eb6e91b0f71fbfa0cf0f9d5a2ed
Brian Weber <brian.weber1337@gmail.com>
2018-09-02 15:47:31 -0400

    moved to fit maven structure

---
+++ src/main/java/edu/uncc/itcs3166/sliding_window/App.java
@@ -0,0 +1,13 @@
+package edu.uncc.itcs3166.sliding_window;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
