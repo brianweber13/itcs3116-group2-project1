4b860d6c49c388840be06558b4ff2b072893c02f
Brian Weber <brian.weber1337@gmail.com>
2018-09-03 15:00:10 -0400

    implemented waitForEvent and to/fromNetworkLayer functions

--- src/main/java/edu/uncc/itcs3166/sliding_window/Framework.java
+++ src/main/java/edu/uncc/itcs3166/sliding_window/Framework.java
@@ -1,5 +1,12 @@
 /**
- * 
+ * Framework simulates the physical and network layers and can be replaced in
+ * order to interface with an actual physical/network layer. We make a few
+ * assumptions:
+ *   - the network layer does not need to be enabled/disabled because we only
+ *     accept input when it is directly asked for (see the fromNetwork function)
+ * This allows us to focus on implementing the data link layer protocols
+ * discussed in chapter 3
+ * @author Brian Weber
  */
 package edu.uncc.itcs3166.sliding_window;
 
@@ -10,14 +17,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.Scanner;
 
-/**
- * Framework provides functions to interface with a mock network layer and
- * physical layer in order to focus on implementing the protocols of the data
- * link layer, as discussed in chapter 3.
- * 
- * @author brian
- */
 public class Framework {
     public enum eventType {
         FRAME_ARRIVAL, CHECKSUM_ERR, TIMEOUT
@@ -26,6 +27,7 @@ public class Framework {
     final static int MAX_PKT = 1024;
     final static String PHYSICAL_LAYER_FILE = "physical-layer";
     final int MAX_SEQUENCE_NUMBER;
+    final int TIMEOUT_IN_MILLIS;
 
     private ObjectOutputStream writer;
     private ObjectInputStream reader;
@@ -34,15 +36,17 @@ public class Framework {
     /**
      * @param mAX_SEQUENCE_NUMBER
      */
-    public Framework(int mAX_SEQUENCE_NUMBER) {
+    public Framework(int mAX_SEQUENCE_NUMBER, int tIMEOUT_IN_MILLIS) {
         super();
         MAX_SEQUENCE_NUMBER = mAX_SEQUENCE_NUMBER;
+        TIMEOUT_IN_MILLIS = tIMEOUT_IN_MILLIS;
         init();
     }
 
     public Framework() {
         super();
         MAX_SEQUENCE_NUMBER = 1;
+        TIMEOUT_IN_MILLIS = 5000;
         init();
     }
 
@@ -75,20 +79,39 @@ public class Framework {
         // }
     }
 
-    // ALL FUNCTIONS JUST RETURN JUNK DATA FOR NOW
     // Wait for an event to happen; return its type in event.
     eventType waitForEvent() {
-        return eventType.TIMEOUT;
+        long now = System.currentTimeMillis();
+        while (true) {
+            try {
+                Thread.sleep(201);
+            } catch (InterruptedException e) {
+                System.out.println("Could not sleep thread...");
+                e.printStackTrace();
+            }
+
+            if (new File(PHYSICAL_LAYER_FILE)
+                    .lastModified() > fileLastModifiedTime) {
+                // TODO: Implement checksumerr here
+                return eventType.FRAME_ARRIVAL;
+            } else if (System.currentTimeMillis() > (now + TIMEOUT_IN_MILLIS)) {
+                return eventType.TIMEOUT;
+            }
+        }
     }
 
     // Fetch a packet from the network layer for transmission on the channel.
     String fromNetworkLayer() {
-        return "";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the data to be sent: ");
+        String data = scanner.nextLine();
+        scanner.close();
+        return data;
     }
 
     // Deliver information from an inbound frame to the network layer.
     void toNetworkLayer(String packet) {
-
+        System.out.println(packet);
     }
 
     // Go get an inbound frame from the physical layer and copy it to r.
@@ -122,6 +145,7 @@ public class Framework {
             writer = new ObjectOutputStream(
                     new FileOutputStream(PHYSICAL_LAYER_FILE));
             writer.writeObject(frameToSend);
+            fileLastModifiedTime = new File(PHYSICAL_LAYER_FILE).lastModified();
             writer.close();
         } catch (IOException ex) {
             System.out.println("Unable to open file '"
@@ -134,32 +158,38 @@ public class Framework {
     }
 
     // Start the clock running and enable the timeout event.
+    // Do we need this?
     void startTime(int k) {
         // TODO: come up with better name than 'k'
 
     }
 
     // Stop the clock and disable the timeout event.
+    // Do we need this?
     void stopTimer(int k) {
 
     }
 
     // Start an auxiliary timer and enable the ack timeout event.
+    // Do we need this?
     void startAckTimer() {
 
     }
 
     // Stop the auxiliary timer and disable the ack timeout event.
+    // Do we need this?
     void stopAckTimer() {
 
     }
 
     // Allow the network layer to cause a network layer ready event.
+    // Do we need this?
     void enableNetworkLayer() {
 
     }
 
     // Forbid the network layer from causing a network layer ready event.
+    // Do we need this?
     void disableNetworkLayer() {
 
     }
