2a30653c1e152c27cbdf5a619a8ecafff88411db
Cam Lengyel <camlengyel@gmail.com>
2018-09-07 14:24:51 -0400

    created senddata and protocol 5 method

--- src/main/java/edu/uncc/itcs3166/sliding_window/GoBackNProtocol.java
+++ src/main/java/edu/uncc/itcs3166/sliding_window/GoBackNProtocol.java
@@ -1,10 +1,79 @@
 package edu.uncc.itcs3166.sliding_window;
 
 import java.util.Scanner;
+
+import edu.uncc.itcs3166.sliding_window.Framework.eventType;
+
 public class GoBackNProtocol {
-    
-    public static void protcol5(){
+    public static final int MAX_SEQ = 5;
+    public static final int TIME_OUT_LENGTH = 5000;
+    private Framework frameWork;
+
+    public GoBackNProtocol(Scanner scannerForGoBackN) {
+        super();
+        frameWork = new Framework(MAX_SEQ, TIME_OUT_LENGTH, scannerForGoBackN);
+
+    }
+
+    public void sendData(int frameNum, int frameExp, String[] packet) {
+        Frame frameToSend = new Frame();
+        frameToSend.setPacket(packet[frameNum]);
+        frameToSend.setSequenceNumber(frameNum);
+        frameToSend
+                .setAcknowledgmentNumber((frameExp + MAX_SEQ) % (MAX_SEQ + 1));
+        frameWork.toPhysicalLayer(frameToSend);
+        frameWork.startTimer(frameNum);
+    }
+
+    public  void protcol5() {
+        int nextFrameToSend;
+        int ackExpected;
+        int frameExpected;
+        Frame r;
+        String [] packet = new String[MAX_SEQ + 1];
+        int nBuffered;
+        int i;
+        eventType event;
         
+        ackExpected= 0;
+        nextFrameToSend= 0;
+        frameExpected= 0;
+        nBuffered= 0;
+        
+        while(true)
+      {
+         event = frameWork.waitForEvent();
+         switch(event)
+         {
+         case FRAME_ARRIVAL:
+         r = frameWork.fromPhysicalLayer();
+         if(r.getSequenceNumber() == frameExpected)
+         {
+             frameWork.toNetworkLayer(r.getPacket());
+             frameWork.inc(ackExpected, MAX_SEQ);
+         }
+         while()
+         {
+             nBuffered = nBuffered - 1;
+             frameWork.stopTimer(ackExpected);
+             frameWork.inc(ackExpected,MAX_SEQ);
+         }
+         break;
+         
+         case TIMEOUT:
+         nextFrameToSend = ackExpected;
+         for(i = 1; i <= nBuffered; i++)
+             {
+             sendData(nextFrameToSend, frameExpected, packet);
+             frameWork.inc(nextFrameToSend, MAX_SEQ);
+             }
+         }
+         packet[nextFrameToSend]= frameWork.fromNetworkLayer();
+         nBuffered = nBuffered+1;
+         sendData(nextFrameToSend,frameExpected,packet);
+         frameWork.inc(nextFrameToSend, MAX_SEQ);
+         
+      }
     }
 
 }
